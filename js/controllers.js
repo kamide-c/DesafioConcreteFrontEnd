e36ccb43c5802475ab1d27bdda76902d676f4d83
Cesar Kamide <cesar@Cesar-2.local>
2015-07-03 23:14:30 -0300

    grid responsivo

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,6 @@ angular.module('starter.controllers', [])
 .controller('ShotsListCtrl', function($scope, dribbble) {
 	var list = "popular";
 	dribbble.list(list).then(function (data){
-        $scope.list = data;
+		$scope.list = data.data.shots;
     })
 })
